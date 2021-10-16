07942da9e934d4079307665fb202a741d322e3af
natarajan <karthicware@gmail.com>
2021-10-16 16:44:42 +0530

    styles modified

--- src/pages-sections/MyAccount/Addresses/SectionAddressForm.js
+++ src/pages-sections/MyAccount/Addresses/SectionAddressForm.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import clsx from 'clsx';
+import clsx from "clsx";
 import axios from "axios";
-import MaskedInput from 'react-text-mask';
+import MaskedInput from "react-text-mask";
 
 //core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,15 +12,15 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import MuiButton from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import MuiButton from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Radio from "@material-ui/core/Radio";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
@@ -47,8 +47,8 @@ import AddLocationRoundedIcon from "@material-ui/icons/AddLocationRounded";
 import EditLocationRoundedIcon from "@material-ui/icons/EditLocationRounded";
 import IconCheck from "@material-ui/icons/Check";
 import IconClose from "@material-ui/icons/Close";
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
 import Check from "@material-ui/icons/Check";
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
@@ -59,7 +59,7 @@ import customCheckboxRadioSwitchStyle from "assets/jss/material-kit-pro-react/cu
 const useStyles = makeStyles({
   ...customCheckboxRadioSwitchStyle,
   bottomControls: {
-    position: 'sticky',
+    position: "sticky",
     zIndex: 1,
     bottom: 0,
     left: 0,
@@ -68,9 +68,9 @@ const useStyles = makeStyles({
     marginTop: 120,
     textAlign: "center",
     //height: 70,
-    display: 'flex',
-    width: '100%'
-  }
+    display: "flex",
+    width: "100%",
+  },
 });
 
 function TextMaskCustom(props) {
@@ -83,7 +83,7 @@ function TextMaskCustom(props) {
         inputRef(ref ? ref.inputElement : null);
       }}
       mask={[/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/]}
-      placeholderChar={'\u2000'}
+      placeholderChar={"\u2000"}
       guide={false}
     />
   );
@@ -96,15 +96,14 @@ TextMaskCustom.propTypes = {
 export default function SectionAddressForm(props) {
   const classes = useStyles();
   const [isDrawerOpen, setDrawerOpen] = React.useState(true);
-  const [addressTypeList, setAddressTypeList] = React.useState([]);
 
   const [name, setName] = React.useState("");
   const [mobile, setMobile] = React.useState("");
+  const [houseNo, setAddress] = React.useState("");
   const [address, setAddress] = React.useState("");
   const [city, setCity] = React.useState("");
   const [state, setState] = React.useState("");
   const [pincode, setPincode] = React.useState("");
-  const [addressType, setAddressType] = React.useState("");
   const [makeAsDefaultAddress, setMakeAsDefaultAddress] = React.useState(false);
 
   const [nameErrMsg, setNameErrMsg] = React.useState(null);
@@ -115,26 +114,10 @@ export default function SectionAddressForm(props) {
   const [pincodeErrMsg, setPincodeErrMsg] = React.useState(null);
 
   React.useEffect(() => {
-    axios
-      .get(`/api/v1/kv/get-all-by-type?groupId=ADDRESS_TYPE`)
-      .then(resp => {
-        if (resp.data) {
-          setAddressTypeList(resp.data);
-          if (!props.edit)
-            setAddressType(resp.data[0].id);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    return function cleanup() { };
-  }, []);
-
-  React.useEffect(() => {
     if (props.edit) {
       axios
         .get(`/api/v1/customer/shipping-address/${props.addressId}`)
-        .then(resp => {
+        .then((resp) => {
           const address = resp.data.result.address;
           setName(address.name);
           setMobile(address.mobile);
@@ -142,17 +125,20 @@ export default function SectionAddressForm(props) {
           setCity(address.city);
           setState(address.state);
           setPincode(address.pincode);
-          setAddressType(address.addressType);
           setMakeAsDefaultAddress(address.makeAsDefaultAddress);
         })
-        .catch(error => {
+        .catch((error) => {
           handleError(error);
         });
     }
   }, [props.edit, props.addressId]);
 
   const toggleDrawer = (open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -176,11 +162,10 @@ export default function SectionAddressForm(props) {
     setCity("");
     setState("");
     setPincode("");
-    setAddressType("");
     setMakeAsDefaultAddress(false);
   };
 
-  const validateName = val => {
+  const validateName = (val) => {
     if (!val || val.length < 3) {
       setNameErrMsg("Invalid Name");
       return false;
@@ -188,7 +173,7 @@ export default function SectionAddressForm(props) {
     setNameErrMsg(null);
     return true;
   };
-  const validateMobile = val => {
+  const validateMobile = (val) => {
     if (!val || val.length < 10) {
       setMobileErrMsg("Invalid Mobile");
       return false;
@@ -196,7 +181,7 @@ export default function SectionAddressForm(props) {
     setMobileErrMsg(null);
     return true;
   };
-  const validateAddress = val => {
+  const validateAddress = (val) => {
     if (!val || val.length < 3) {
       setAddressErrMsg("Required");
       return false;
@@ -204,7 +189,7 @@ export default function SectionAddressForm(props) {
     setAddressErrMsg(null);
     return true;
   };
-  const validateCity = val => {
+  const validateCity = (val) => {
     if (!val || val.length < 3) {
       setCityErrMsg("Invalid City");
       return false;
@@ -212,7 +197,7 @@ export default function SectionAddressForm(props) {
     setCityErrMsg(null);
     return true;
   };
-  const validateState = val => {
+  const validateState = (val) => {
     if (!val || val.length < 3) {
       setStateErrMsg("Invalid State");
       return false;
@@ -220,7 +205,7 @@ export default function SectionAddressForm(props) {
     setStateErrMsg(null);
     return true;
   };
-  const validatePincode = val => {
+  const validatePincode = (val) => {
     if (!val || val.length < 6 || isNaN(val)) {
       setPincodeErrMsg("Invalid Pincode");
       return false;
@@ -247,14 +232,14 @@ export default function SectionAddressForm(props) {
           city,
           state,
           pincode,
-          addressType,
-          defaultAddress: makeAsDefaultAddress
+          houseNo,
+          defaultAddress: makeAsDefaultAddress,
         })
-        .then(resp => {
+        .then((resp) => {
           props.onSuccessUpdateAddress(resp.data.result);
           clear();
         })
-        .catch(error => {
+        .catch((error) => {
           handleError(error);
         });
     } else {
@@ -266,14 +251,14 @@ export default function SectionAddressForm(props) {
           city,
           state,
           pincode,
-          addressType,
-          defaultAddress: makeAsDefaultAddress
+          houseNo,
+          defaultAddress: makeAsDefaultAddress,
         })
-        .then(resp => {
+        .then((resp) => {
           props.onSuccessAddAddress(resp.data.result);
           clear();
         })
-        .catch(error => {
+        .catch((error) => {
           handleError(error);
         });
     }
@@ -289,21 +274,37 @@ export default function SectionAddressForm(props) {
       >
         <GridContainer style={{ width: 400, margin: 0 }}>
           <GridItem>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: 10 }}>
-              {props.edit ? <Typography variant="h6">
-                <EditLocationRoundedIcon style={{ marginBottom: -5, marginRight: 5 }} />
-                Edit Address
-              </Typography>
-                :
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: 10,
+              }}
+            >
+              {props.edit ? (
                 <Typography variant="h6">
-                  <AddLocationRoundedIcon style={{ marginBottom: -5, marginRight: 5 }} />
-              Add New Address
-            </Typography>}
+                  <EditLocationRoundedIcon
+                    style={{ marginBottom: -5, marginRight: 5 }}
+                  />
+                  Edit Address
+                </Typography>
+              ) : (
+                <Typography variant="h6">
+                  <AddLocationRoundedIcon
+                    style={{ marginBottom: -5, marginRight: 5 }}
+                  />
+                  Add New Address
+                </Typography>
+              )}
 
-              <IconButton aria-label="delete" onClick={() => {
-                clear();
-                props.onCloseModal();
-              }}>
+              <IconButton
+                aria-label="delete"
+                onClick={() => {
+                  clear();
+                  props.onCloseModal();
+                }}
+              >
                 <CloseRoundedIcon />
               </IconButton>
             </div>
@@ -312,12 +313,12 @@ export default function SectionAddressForm(props) {
             <CustomInput
               labelText="Name *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               autoFocus
               inputProps={{
                 value: name,
-                onChange: e => {
+                onChange: (e) => {
                   setName(e.target.value);
                   validateName(e.target.value);
                 },
@@ -326,18 +327,22 @@ export default function SectionAddressForm(props) {
               helperText="test"
               error={nameErrMsg ? true : false}
             />
-            {nameErrMsg && <Danger>{nameErrMsg}</Danger>}
+            {nameErrMsg && (
+              <Danger>
+                <Typography variant="caption">{nameErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12}>
             <CustomInput
               labelText="Mobile *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               inputProps={{
                 value: mobile,
-                onChange: e => {
+                onChange: (e) => {
                   setMobile(e.target.value);
                   validateMobile(e.target.value);
                 },
@@ -347,134 +352,122 @@ export default function SectionAddressForm(props) {
               }}
               helperText="test"
               error={mobileErrMsg ? true : false}
-              startAdornment={<InputAdornment position="start">+91 - </InputAdornment>}
+              startAdornment={
+                <InputAdornment position="start">+91 - </InputAdornment>
+              }
               inputComponent={TextMaskCustom}
             />
-            {mobileErrMsg && <Danger>{mobileErrMsg}</Danger>}
+            {mobileErrMsg && (
+              <Danger>
+                <Typography variant="caption">{mobileErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12}>
             <CustomInput
               labelText="Address (Flat, House No., Building, Street, Area) *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               inputProps={{
                 value: address,
-                onChange: e => {
+                onChange: (e) => {
                   setAddress(e.target.value);
                   validateAddress(e.target.value);
                 },
-                error: addressErrMsg ? true : false
+                error: addressErrMsg ? true : false,
               }}
               helperText="test"
               error={addressErrMsg ? true : false}
             />
-            {addressErrMsg && <Danger>{addressErrMsg}</Danger>}
+            {addressErrMsg && (
+              <Danger>
+                <Typography variant="caption">{addressErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12}>
             <CustomInput
               labelText="Town/City *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               inputProps={{
                 value: city,
-                onChange: e => {
+                onChange: (e) => {
                   setCity(e.target.value);
                   validateCity(e.target.value);
                 },
-                error: cityErrMsg ? true : false
+                error: cityErrMsg ? true : false,
               }}
               helperText="test"
               error={cityErrMsg ? true : false}
             />
-            {cityErrMsg && <Danger>{cityErrMsg}</Danger>}
+            {cityErrMsg && (
+              <Danger>
+                <Typography variant="caption">{cityErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12}>
             <CustomInput
               labelText="State *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               inputProps={{
                 value: state,
-                onChange: e => {
+                onChange: (e) => {
                   setState(e.target.value);
                   validateState(e.target.value);
                 },
-                error: stateErrMsg ? true : false
+                error: stateErrMsg ? true : false,
               }}
               helperText="test"
               error={stateErrMsg ? true : false}
             />
-            {stateErrMsg && <Danger>{stateErrMsg}</Danger>}
+            {stateErrMsg && (
+              <Danger>
+                <Typography variant="caption">{stateErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12}>
             <CustomInput
               labelText="Pincode *"
               formControlProps={{
-                fullWidth: true
+                fullWidth: true,
               }}
               inputProps={{
                 value: pincode,
-                onChange: e => {
+                onChange: (e) => {
                   setPincode(e.target.value);
                   validatePincode(e.target.value);
                 },
                 error: pincodeErrMsg ? true : false,
                 minLength: 6,
-                maxLength: 6
+                maxLength: 6,
               }}
               helperText="test"
               error={pincodeErrMsg ? true : false}
             />
-            {pincodeErrMsg && <Danger>{pincodeErrMsg}</Danger>}
-          </GridItem>
-
-          <GridItem md={12}>
-            <Typography variant="body1" style={{ paddingTop: 20, paddingBottom: 20 }}>Address Type</Typography>
-            <div
-              className={
-                classes.checkboxAndRadio + " " + classes.checkboxAndRadioHorizontal
-              }
-            >
-              {addressTypeList.map(type => (
-                <FormControlLabel
-                  key={type.id}
-                  control={
-                    <Radio
-                      checked={addressType === type.id}
-                      onChange={() => setAddressType(type.id)}
-                      value={type.id}
-                      name="addressType"
-                      aria-label={type.id}
-                      icon={<FiberManualRecord className={classes.radioUnchecked} />}
-                      checkedIcon={
-                        <FiberManualRecord className={classes.radioChecked} />
-                      }
-                      classes={{
-                        checked: classes.radio,
-                        root: classes.radioRoot
-                      }}
-                    />
-                  }
-                  classes={{
-                    label: classes.label
-                  }}
-                  label={type.text}
-                />
-              ))}
-            </div>
+            {pincodeErrMsg && (
+              <Danger>
+                <Typography variant="caption">{pincodeErrMsg}</Typography>
+              </Danger>
+            )}
           </GridItem>
 
           <GridItem md={12} style={{ marginTop: 20 }}>
             <div
               className={
-                classes.checkboxAndRadio + " " + classes.checkboxAndRadioHorizontal
+                classes.checkboxAndRadio +
+                " " +
+                classes.checkboxAndRadioHorizontal
               }
             >
               <FormControlLabel
@@ -485,7 +478,7 @@ export default function SectionAddressForm(props) {
                     icon={<Check className={classes.uncheckedIcon} />}
                     classes={{
                       checked: classes.checked,
-                      root: classes.checkRoot
+                      root: classes.checkRoot,
                     }}
                     checked={makeAsDefaultAddress}
                   />
@@ -495,7 +488,6 @@ export default function SectionAddressForm(props) {
               />
             </div>
           </GridItem>
-
         </GridContainer>
         <div className={classes.bottomControls}>
           <Button block onClick={() => submit()} color="warning">
@@ -503,22 +495,22 @@ export default function SectionAddressForm(props) {
             Save
           </Button>
           {props.clear ? (
-            <Button block onClick={() => clear()} >
+            <Button block onClick={() => clear()}>
               <IconClose />
               Clear
             </Button>
           ) : (
-              <Button
-                block
-                onClick={() => {
-                  clear();
-                  props.onCloseModal();
-                }}
-              >
-                <IconClose />
+            <Button
+              block
+              onClick={() => {
+                clear();
+                props.onCloseModal();
+              }}
+            >
+              <IconClose />
               Cancel
-              </Button>
-            )}
+            </Button>
+          )}
         </div>
       </SwipeableDrawer>
     </div>
